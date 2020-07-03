b896c1ce6719348f5a3e963192155746a397236b
salwks <stra2003@gmail.com>
2020-07-03 16:45:42 +0900

    Edit-Profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,15 @@
 import express from "express";
+import {
+  userDetail,
+  editProfile,
+  changePassword,
+} from "../controllers/userControllers";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
+import routes from "../routes";
+import {
+  upload,
+  videoDetail,
+  editVideo,
+} from "../controllers/videoControllers";
 
 const videoRouter = express.Router();
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
 
 export default videoRouter;
