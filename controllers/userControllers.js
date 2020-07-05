a5387492f1193c184e779b567c2def98758bebe1
salwks <stra2003@gmail.com>
2020-07-05 23:18:18 +0900

    ready for frontend

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,33 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, mail, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+  } else {
+    //사용자등록
+    //사용자 로그인
+    res.redirect(routes.home);
+  }
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  //로그아웃 처리
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
