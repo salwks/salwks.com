c2a45b8c952a6910c98a682d227270693138c26d
salwks <stra2003@gmail.com>
2020-07-05 12:29:05 +0900

    add db mixins

---
+++ db.js
@@ -0,0 +1,67 @@
+export const videos = [
+  {
+    id: 1111,
+    title: "Video1",
+    description: "this is test1",
+    views: 24,
+    videoFile:
+      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4",
+    creator: {
+      id: "salwks",
+      name: "salwks",
+      email: "stra2003@gmail.com",
+    },
+  },
+  {
+    id: 1112,
+    title: "Video2",
+    description: "this is test2",
+    views: 24,
+    videoFile:
+      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4",
+    creator: {
+      id: "salwks",
+      name: "salwks",
+      email: "stra2003@gmail.com",
+    },
+  },
+  {
+    id: 1113,
+    title: "Video3",
+    description: "this is test3",
+    views: 24,
+    videoFile:
+      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4",
+    creator: {
+      id: "salwks",
+      name: "salwks",
+      email: "stra2003@gmail.com",
+    },
+  },
+  {
+    id: 1114,
+    title: "Video4",
+    description: "this is test4",
+    views: 24,
+    videoFile:
+      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4",
+    creator: {
+      id: "salwks",
+      name: "salwks",
+      email: "stra2003@gmail.com",
+    },
+  },
+  {
+    id: 1115,
+    title: "Video5",
+    description: "this is test5",
+    views: 24,
+    videoFile:
+      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4",
+    creator: {
+      id: "salwks",
+      name: "salwks",
+      email: "stra2003@gmail.com",
+    },
+  },
+];
