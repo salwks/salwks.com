05b303285626c52a9e3cd3e8080c7fdada4674ae
salwks <stra2003@gmail.com>
2020-07-07 14:56:40 +0900

    mongodb:add to Model

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/salwks", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
