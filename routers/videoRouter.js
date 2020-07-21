09f9b04d3cffddb4c109cee03e3a1bb33c97ab26
salwks <stra2003@gmail.com>
2020-07-21 12:07:58 +0900

    delete Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,18 +2,25 @@ import express from "express";
 import routes from "../routes";
 import {
   videoDetail,
-  editVideo,
   deleteVideo,
   getUpload,
   postUpload,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoControllers";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
+
+//업로드
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+//상세
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+//수정
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+//삭제
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
